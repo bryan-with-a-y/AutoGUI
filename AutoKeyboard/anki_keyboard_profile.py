c3d0b6f4410637bb0eaa0a3b64d78641a64a84df
Bryan Klouda <bryan.klouda@gmail.com>
2020-10-21 17:24:08 -0400

    uses a sequence calling function to reduce number of hard coded rest calls. adds a main function. changes rest between calls to 1.2 from 1.0

--- AutoKeyboard/anki_keyboard_profile.py
+++ AutoKeyboard/anki_keyboard_profile.py
@@ -11,50 +11,36 @@ def strike_sequence(lst):
             rest(3)
             continue
         keyboard.press_and_release(key_stroke)
-        rest()
+        rest(1.2)
 
 def parse_word():
     rest(5)
-    keyboard.press_and_release("ctrl+c")
-    rest()
-    keyboard.press_and_release("tab")
-    rest()
-    keyboard.press_and_release("ctrl+v")
-    rest()
-    keyboard.press_and_release("shift+tab")
-    rest()
-    keyboard.press_and_release("shift+tab")
-    rest()
-    keyboard.press_and_release("shift+tab")
-    rest()
-    keyboard.press_and_release("ctrl+v")
-    rest()
-    keyboard.press_and_release("tab")
+    sequence = ["ctrl+c", "tab", "ctrl+v", "shift+tab", "shift+tab",
+                "shift+tab", "ctrl+v", "tab"]
+    strike_sequence(sequence)
 
 def edit_last_word():
     rest(5)
-    keyboard.press_and_release("ctrl+h")
-    rest()
-    keyboard.press_and_release("enter")
-    rest()
-    keyboard.press_and_release("enter")
+    sequence = ["ctrl+h", "enter", "enter"]
+    strike_sequence(sequence)
 
 def enter_card():
     rest(3.5)
     sequence = ["tab", "ctrl+a", "ctrl+c", "ctrl+enter", "rest",
                 "tab", "tab", "ctrl+v"]
     strike_sequence(sequence)
-    #keyboard.press_and_release("tab")
-    #keyboard.press_and_release("ctrl+a")
-    #keyboard.press_and_release("ctrl+c")
-    #keyboard.press_and_release("ctrl+enter")
-    #keyboard.press_and_release("tab")
-    #keyboard.press_and_release("tab")
-    #keyboard.press_and_release("ctrl+v")
 
-keyboard.add_hotkey('ctrl+shift+p', lambda: parse_word())
-keyboard.add_hotkey('ctrl+shift+h', lambda: edit_last_word())
-keyboard.add_hotkey('ctrl+shift+enter', lambda: enter_card())
+def minimize_window():
+    keyboard.press_and_release("windows+down")
 
-keyboard.wait("esc")
+def main():
+    keyboard.add_hotkey('ctrl+shift+p', lambda: parse_word())
+    keyboard.add_hotkey('ctrl+shift+h', lambda: edit_last_word())
+    keyboard.add_hotkey('ctrl+shift+enter', lambda: enter_card())
+
+    print("Press esc to exit keyboard profile")
+    minimize_window()
+    keyboard.wait("esc")
+
+main()
 
